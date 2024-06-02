5826289c55a7403579d0d8b9db6a7cb0bcf4953f
Jesus Gastelum <gastelumtec21@gmail.com>
2024-06-02 11:03:47 -0700

    packages endpoints working

--- backend/src/domain/repositories/packages.repository.ts
+++ backend/src/domain/repositories/packages.repository.ts
@@ -2,7 +2,7 @@ import { PackageEntity } from "../entities/package.entity";
 
 export abstract class PackagesRepository {
 
-  abstract getAll(): Promise<PackageEntity>;
+  abstract getAll(): Promise<PackageEntity[]>;
 
   abstract getById(id: number): Promise<PackageEntity>;
 
--- backend/src/infraestructure/repositories/package.repository.impl.ts
+++ backend/src/infraestructure/repositories/package.repository.impl.ts
@@ -1,12 +1,33 @@
+import { Package } from "../../db/postgres/models";
 import { PackageEntity } from "../../domain/entities/package.entity";
 import { PackagesRepository } from "../../domain/repositories/packages.repository";
 
 export class PackageRepositoryImpl implements PackagesRepository {
-  getAll(): Promise<PackageEntity> {
-    throw new Error("Method not implemented.");
+  async getAll(): Promise<PackageEntity[]> {
+    const packages = await Package.find();
+
+    const packagesEntities = packages.map(item => (
+      new PackageEntity(
+        item.id,
+        item.name,
+        item.price,
+        item.description
+      )
+    ));
+
+    return packagesEntities;
   }
-  getById(id: number): Promise<PackageEntity> {
-    throw new Error("Method not implemented.");
+  
+  async getById(id: number): Promise<PackageEntity> {
+    const packageDb = await Package.findOne({
+      where: {
+        id
+      }
+    });
+
+    if (!packageDb) throw new Error('Paquete no encontrado');
+
+    return new PackageEntity(packageDb.id, packageDb?.name, packageDb?.price, packageDb?.description);
   }
   
 }
