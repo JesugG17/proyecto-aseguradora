e33e088904e7f9520ea12060b81e93001a87c6d4
Jesus Gastelum <gastelumtec21@gmail.com>
2024-06-04 17:18:39 -0700

    merge branch main

---
+++ frontend/src/pages/CreditCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <div class="header" />
+    <form>
+      
+    </form>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    width: 90%;
+    max-width: 900px;
+    margin: 20px auto;
+    border-radius: 24px;
+    background: #fff;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+
+  .header {
+    height: 50px;
+    background: linear-gradient(to left, #2ECC7180, #A3E4D7);
+    border-radius: 24px 24px 0px 0px;
+  }
+
+
+</style>
